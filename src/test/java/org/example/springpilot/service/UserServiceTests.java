94bb773f1ef9235c830e79a92d04f813d4512014
davshikhar <shikharyadav900@gmail.com>
2026-03-01 23:50:37 +0530

    Added testing logic

---
+++ src/test/java/org/example/springpilot/service/UserServiceTests.java
@@ -0,0 +1,14 @@
+package org.example.springpilot.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class UserServiceTests {
+
+    @Test
+    public void testAdd(){
+        assertEquals();
+    }
+}
