cd18633a341f1d6f336212030232ae2df0e6d4f4
davshikhar <shikharyadav900@gmail.com>
2026-03-31 23:16:42 +0530

    Made small change to the code

--- src/main/java/org/example/springpilot/Controller/GoogleAuthController.java
+++ src/main/java/org/example/springpilot/Controller/GoogleAuthController.java
@@ -1,15 +1,13 @@
 package org.example.springpilot.Controller;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.example.springpilot.Entity.User;
 import org.example.springpilot.Repository.UserEntryRepo;
 import org.example.springpilot.Service.UserDetailsServiceImpl;
-import org.example.springpilot.Utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.LinkedMultiValueMap;
@@ -19,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.example.springpilot.Utils.JwtUtil;
 
 import java.util.*;
 
@@ -26,6 +25,12 @@ import java.util.*;
 @RequestMapping("/auth/google")
 @Slf4j
 public class GoogleAuthController {
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -37,67 +42,51 @@ public class GoogleAuthController {
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    private UserEntryRepo userEntryRepo;
-
-    @Value("${spring.security.oauth2.client.registration.google.client-id}")
-    private String clientId;
-
-    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
-    private String clientSecret;
+    private UserEntryRepo userRepository;
 
     @Autowired
     private JwtUtil jwtUtil;
 
     @GetMapping("/callback")
-    public ResponseEntity<?> handleGoogleCallback(@RequestParam String code){
-        try{
+    public ResponseEntity<?> handleGoogleCallback(@RequestParam String code) {
+        try {
             String tokenEndpoint = "https://oauth2.googleapis.com/token";
             MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-            params.add("code",code);
-            params.add("client_id",clientId);
-            params.add("client_secret",clientSecret);
-            params.add("redirect_uri","https://developers.google.com/oauthplayground");
-            params.add("grant_type","authorization_code");
-
+            params.add("code", code);
+            params.add("client_id", clientId);
+            params.add("client_secret", clientSecret);
+            params.add("redirect_uri", "https://developers.google.com/oauthplayground");
+            params.add("grant_type", "authorization_code");
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-            HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(params,headers);
-
-            ResponseEntity<Map> response = restTemplate.postForEntity(tokenEndpoint, request, Map.class);
-            String accessToken = (String) response.getBody().get("id_token");
-            String userInfoRequest = "https://oauth2.googleapis.com/tokeninfo?id_token="+accessToken;
-            ResponseEntity<Map> userInfoResponse = restTemplate.getForEntity(userInfoRequest, Map.class);
-            if(userInfoResponse.getStatusCode() == HttpStatus.OK){
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+            ResponseEntity<Map> tokenResponse = restTemplate.postForEntity(tokenEndpoint, request, Map.class);
+            String idToken = (String) tokenResponse.getBody().get("id_token");
+            String userInfoUrl = "https://oauth2.googleapis.com/tokeninfo?id_token=" + idToken;
+            ResponseEntity<Map> userInfoResponse = restTemplate.getForEntity(userInfoUrl, Map.class);
+            if (userInfoResponse.getStatusCode() == HttpStatus.OK) {
                 Map<String, Object> userInfo = userInfoResponse.getBody();
                 String email = (String) userInfo.get("email");
                 UserDetails userDetails = null;
                 try{
                     userDetails = userDetailsService.loadUserByUsername(email);
-                }
-                catch(Exception e){
-                    log.error("User not found so creating a new user",e);
-                    User user = new User();
+                }catch (Exception e){
+                    User user = new org.example.springpilot.Entity.User();
                     user.setEmail(email);
-                    user.setUsername((String)userInfo.get("name"));
-                    user.setRoles(Arrays.asList("USER"));
-                    user.setSentimentAnalysis(false);
+                    user.setUsername(email);
                     user.setPassword(passwordEncoder.encode(UUID.randomUUID().toString()));
-                    userEntryRepo.save(user);
+                    user.setRoles(Arrays.asList("USER"));
+                    userRepository.save(user);
                     userDetails = userDetailsService.loadUserByUsername(email);
                 }
-                    /// now save the user in spring security context
-//                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-//                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                /// here instead of adding the authentication to SecurityContextHolder we must add a jwt token generator.
-            String jwtToken = jwtUtil.generateToken(userDetails);
-                return ResponseEntity.ok(Collections.singletonMap("token",jwtToken));
+                String jwtToken = jwtUtil.generateToken(userDetails);
+                return ResponseEntity.ok(Collections.singletonMap("token", jwtToken));
             }
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        catch(Exception e){
-            log.error("Exception occured while logging in",e);
+        } catch (Exception e) {
+            log.error("Exception occurred while handleGoogleCallback ", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+
     }
 }
